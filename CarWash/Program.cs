fd906d7bff83f9973dd7682ee92750937f803009
MakaraRean <r.makara232@gmail.com>
2021-09-24 02:52:50 +0700

    Carwash

--- CarWash/Program.cs
+++ CarWash/Program.cs
@@ -22,7 +22,7 @@ namespace CarWash
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new create_users());
-        }
+        }bool
         [System.Runtime.InteropServices.DllImport("user32.dll")]
         private static extern bool SetProcessDPIAware();
     }
